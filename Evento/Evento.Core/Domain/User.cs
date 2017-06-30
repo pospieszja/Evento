c75288307ce486fa641b109097207deaeae7ff53
pospieszja <j.pospieszynski@outloook.com>
2017-06-30 15:17:29 +0200

    Add User class

---
+++ Evento/Evento.Core/Domain/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Evento.Core.Domain
+{
+    class User: Entity
+    {
+        public string Name { get; protected set; }
+        public string Email { get; protected set; }
+        public string Password { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+    }
+}
