077bebb1b0bf8239e6f33bba0bd4132315667d22
pospieszja <j.pospieszynski@outloook.com>
2017-06-30 15:09:26 +0200

    Add Event class

---
+++ Evento/Evento.Core/Domain/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Evento.Core.Domain
+{
+    class Event: Entity
+    {
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public DateTime StartDate { get; protected set; }
+        public DateTime EndDate { get; protected set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
